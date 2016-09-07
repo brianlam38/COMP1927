65cfe3de89989db685a091cc5d9d97eff1cd7965
brianlam38 <brian.lam38@gmail.com>
2016-09-07 13:17:06 +1000

    updating Queue notes

--- 3.Queues/Queue.c
+++ 3.Queues/Queue.c
@@ -23,7 +23,7 @@ Queue newQueue(int length)
 	new->size = length;						     // initialise size of queue
 	new->nitems = 0;							 // initially empty
 	new->head = 0;								 // location starting at 0
-	new->tail = -1;							     // location starting at -1
+	new->tail = -1;							     // tail needs to start at -1 as we move tail +1 position in enqueue()
 	new->data = malloc(length*sizeof(Item));	 // malloc space for array (storing queue data)
 	assert(new->data != NULL);					 // check that data is pointing to nothing		
 	return new;
@@ -40,20 +40,20 @@ void dropQueue(Queue q)
 // add item onto queue
 void enqueue(Queue q, Item it)
 {
-	assert(q->nitems < q->size);
-	q->tail = (q->tail+1)%q->size;
-	q->data[q->tail] = it;
-	q->nitems++;
+	assert(q->nitems < q->size);	// check queue isn't already full (otherwise can't enqueue)
+	q->tail = (q->tail+1)%q->size;  // move tail onto next location
+	q->data[q->tail] = it;			// copy new item into that particular location
+	q->nitems++;					// increment num of items
 }
  
 // remove item from queue
 Item dequeue(Queue q)
 {
-	assert(q->nitems > 0);
-	Item it = q->data[q->head];
-	q->head = (q->head+1)%q->size;
-	q->nitems--;
-	return it;
+	assert(q->nitems > 0);			// check num of items in queue > 0
+	Item it = q->data[q->head];		// grab head item (FIFO, so remove 1st item)
+	q->head = (q->head+1)%q->size;	// advance head ptr (Advance head ptr by 1 position)
+	q->nitems--;					// decrement item count
+	return it;						// return dequeued item
 }
  
 // queue contains no items?
@@ -79,6 +79,7 @@ void show(Queue q)
 	}
 	for (i = q->head; i != q->tail; i = (i+1)%q->size)
 	// increment: mod size of queue, so it will go back to index 0
-		printf("%d->",q->data[i]);
-		printf("%d@%d\n",q->data[i],i);
+		printf("%d->",q->data[i]);		// print out data
+	// print out final data
+	printf("%d@%d\n",q->data[i],i); 
 }
