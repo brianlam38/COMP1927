055c81d182f1b284a163c3c7a03c21c03c56a54e
brianlam38 <brian.lam38@gmail.com>
2016-09-21 15:36:08 +1000

    adding pastPlays generator

---
+++ pastPlaysGen/pastPlaysGen.c
@@ -0,0 +1,23 @@
+// This program creates a .txt file which contains a string
+// of pastPlays which can be used for the COMP1927 Dracula game.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+	// Generate a string of 7 characters at a time, space separated,
+	// 'A'-'Z' or '?' or '.' which resemble the 7 moves / encounters
+	// that players will make throughout the duration of a Dracula.
+	// game.
+
+	// This string will follow all Dracula game rules.
+
+	// Insert this string into a .txt file.
+	
+	// The .txt file can be used as input into the views, to help
+	// test and determine AI moves.
+
+	return EXIT_SUCCESS;
+}
+
