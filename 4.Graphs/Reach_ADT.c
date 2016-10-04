0097ca0a7fb89ceaacd3c2f9ff35038b0d51053a
brianlam38 <brian.lam38@gmail.com>
2016-10-04 13:17:28 +1100

    adding Reachibility ADT

---
+++ 4.Graphs/Reach_ADT.c
@@ -0,0 +1,28 @@
+// ################################
+// REACHIBILITY MATRIX - ADT FORMAT
+// ################################
+
+int reachable(Graph g, Vertex src, Vertex dest) {	// Check reachability
+	if (g->tc == NULL) makeClosure(g);
+	return g->tc[src][dest];
+}
+
+void makeClosure(Graph g) {
+	int i, s, t, v = g->nV;
+	int **tc = makeMatrix(V, V, 0);					// Set up matrix, initialise values 0
+
+	for (s = 0; s < V; s++) {						// Copy adj matrix values to tc matrix
+		for (t = 0; t < V; t++) {
+			tc[s][t] = g->edges[s][t];
+		}
+	}
+	for (i = 0; i < V; i++) {						// Warshall's Algorithm
+		for (s = 0; s < V; s++) {
+			if (tc[i][s] == 0) continue;
+			for (t = 0; t < V; t++) {
+				if (tc[i][t] == 1) tc[s][t] = 1;
+			}
+		}
+	}
+	g->tc = tc;										// Set graph tc ptr = tc matrix
+}
