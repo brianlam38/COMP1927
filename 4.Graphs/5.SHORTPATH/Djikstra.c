8b7156c5aa6d81f707658464b58316a58f8e9183
brianlam38 <brian.lam38@gmail.com>
2016-10-12 14:15:23 +1100

    updating Djikstras algo notes

--- 4.Graphs/5.SHORTPATH/Djikstra.c
+++ 4.Graphs/5.SHORTPATH/Djikstra.c
@@ -1,3 +1,7 @@
+// ############################
+// DJIKSTRA'S ALGO - PSEUDOCODE
+// ############################
+
 void shortestPath(Graph g, Vertex s) {
 	VertexSet vSet = {s};				// init start vertex S
 	float dist[nV(g)] = {inf,inf,...};	// malloc dist[] and init as infinity
@@ -21,3 +25,41 @@ void shortestPath(Graph g, Vertex s) {
 	}
 	// dist[] and pred[] are now set appropriately
 }
+
+// ###############################
+// DJIKSTRA'S ALGO - PQUEUE METHOD
+// ###############################
+
+void shortestPath(Graph g, Vertex start, Vertex pred[], float dist[]) {
+	PQueue pq = newPQ(dist,nV(g));			// create PQ based on dist[] -> Inside PQ data structure,
+											// store pointer to dist[]. When working out priority,
+											// it will access array to find cheapest one.
+	for (Vertex v = 0; v < nV(g); v++) {
+		pred[v] = -1;						// init pred[] values to -1
+		dist[v] = MAX_WT;					// init dist[] values to MAX_WEIGHT
+		join(pq,v);							// add V to PQ
+	}
+	dist[start] = 0.0;						// init dist[S] = 0
+	reorder(pq,start);						// reorder PQ -> S moves to the front (has priority)
+
+	while (!empty(pq)) {
+		Vertex S = leave(pq);				// pop S off PQueue / grab cheapest vertex
+		/* (1) ITERATE THROUGH NEIGHBOURS */
+		for (Vertex t = 0; t < nV(g); t++) {// iterate through other vertices
+			float len = g->adj[s][t];		// find a vertex T connected with vertex S (neighbours)
+			if (len == NO_EDGE) continue;	// no edge found, continue through next iteration
+			/* (2) EDGE RELAXATION */
+			if (dist[s]+len < dist[t]) {	// if cost of path < known cost of path
+				pred[t] = s;				// set pred[t] = S
+				dist[t] = dist[s]+len;		// update new shortest dist
+				/* (3) FIX PQ AS T-WEIGHT UPDATED */
+				reorder(pq,t);
+			}
+		}
+	}
+}
+
+
+
+
+
