b90bee471853b52c407d638b77150772fb1b580b
brianlam38 <brian.lam38@gmail.com>
2016-10-25 17:08:17 +1100

    adding BST traversal notes

--- 5.BSTs/traverse.c
+++ 5.BSTs/traverse.c
@@ -16,3 +16,30 @@ void showBSTreeBF(BSTree t)
 	}
 	printf("\n");
 }
+
+/* 	Traversal with visit operation
+	void(*visit)(item) = Do something when we visit an item
+	Example: Print something out, compute something etc.
+	Visit function: Takes item, does something to it, no returns.
+*/
+void Traverse(Tree t, void (*visit)(Item)) {
+	if (t != NULL) {
+		// visit data in root node FIRST = NLR (prefix)
+		Traverse(t->left, visit);
+		// visit data in root node MIDDLE = LNR (infix)
+		Traverse(t->right,visit);
+		// visit data in root node LAST = LRN (postfix)
+ 	}
+ 	// Level-order cannot be implemented recursively
+}
+
+/* Passing ptr into function
+
+   void(*visit)(item)
+
+   *visit = ptr to a function with parameter of type (Item)
+   void(*visit)(item) = the function returns nothing
+*/
+
+
+
