3e8af524940eb34a75e76e716b0d6b19213c0786
brianlam38 <brian.lam38@gmail.com>
2016-10-28 14:02:23 +1100

    adding Stack notes

--- 3.Stacks/Stack.c
+++ 3.Stacks/Stack.c
@@ -1,10 +1,17 @@
 // Stack ADT interface
 
+// ##########################
+// STACK REP AS A LINKED LIST
+// ##########################
+
+// Items = char
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include "Stack.h"
 
+typedef char Item;
 typedef struct Node *Link;
 typedef struct Node { Item value; Link next; } Node;
 typedef struct StackRep { int nitems; Link items; } StackRep;
@@ -12,20 +19,20 @@ typedef struct StackRep { int nitems; Link items; } StackRep;
 // Stack validity check
 static int isValidStack(Stack s)
 {
-	if (s == NULL) return 0;
-	if (s->nitems < 0) return 0;
-	if (s->nitems > 0 && s->items == NULL) return 0;
+	if (s == NULL) return 0;		// empty stack, not valid
+	if (s->nitems < 0) return 0;	// nitems < 0, not valid
+	if (s->nitems > 0 && s->items == NULL) return 0;	// nitems = null, not valid
 	Link cur; int nitems = 0;
-	for (cur = s->items; cur != NULL; cur = cur->next)
-		nitems++;
-	if (nitems != s->nitems) return 0;
-	// couldn't find anything wrong
-	return 1;
+	for (cur = s->items; cur != NULL; cur = cur->next)	// loop through stack
+		nitems++;											// count items in stack
+	if (nitems != s->nitems) return 0;					// if nitems doesn't match count, not valid
+
+	return 1;	// passed call conditions, valid stack.
 }
 
 // add item onto stack
-// Pre: valid(Stack,s) // && !isFull(s)
-// Post: s' == it:s
+// Pre-condition: valid(Stack,s) // && !isFull(s)
+// Post-condition: s' == it:s 	(s' = new value of stack at end of the FN)
 void push(Stack s, Item it)
 {
 	assert(isValidStack(s));
