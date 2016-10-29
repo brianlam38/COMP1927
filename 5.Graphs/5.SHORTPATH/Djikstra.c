953976115c113b05b440bd92527855711945473c
brianlam38 <brian.lam38@gmail.com>
2016-10-29 12:49:54 +1100

    updating all files

--- 5.Graphs/5.SHORTPATH/Djikstra.c
+++ 5.Graphs/5.SHORTPATH/Djikstra.c
@@ -1,22 +1,38 @@
-// ############################
-// DJIKSTRA'S ALGO - PSEUDOCODE
-// ############################
+// ###############
+// DJIKSTRA'S ALGO - PSUEDOCODE
+// ###############
+
+declare + init vSet to {start}    	  // start = source v
+declare dist[]
+init all dist[] values = infinity 	  // infinity = impossible value
+while (any edges left to consider) {
+	choose an edge e = {s,t}
+		where s is within vSet
+		and e is the min weight edge
+	relax along e, add t to vSet
+}
+
+// ###############
+// DJIKSTRA'S ALGO
+// ###############
+
+// vSet = set of vertices which we have seen so far	
 
 void shortestPath(Graph g, Vertex s) {
 	VertexSet vSet = {s};				// init start vertex S
-	float dist[nV(g)] = {inf,inf,...};	// malloc dist[] and init as infinity
-	dist[s] = 0;						// init S distance = 0
-	Vertex pred[nV(g)] = {-1,-1,...};	// malloc pred[] and init as -1 (impossible vertex value)
-	Edge eSet = edges(g);			// create set of edges
-
+	float dist[nV(g)] = {inf,inf,...};	// MALLOC dist[] and init as infinity
+	dist[s] = 0;						// set dist[s] = 0
+	Vertex pred[nV(g)] = {-1,-1,...};	// MALLOC pred[] and init as -1 (impossible vertex value)
+	Edge eSet = edges(g);				// create set of edges
+	/* FIND NEXT EDGE (while eSet !empty) */
 	while (!empty(eSet)) {
 		find (Edge e) satisfying {      // find an edge satisfying the following properties:
-			e = (s,t,w)						// S within vSet			
+			e = (s,t,w)						// S within vSet (something already visited)			
 		}									// w is mininum weight of all such edges
 		/* EDGE RELAXATION */
-		if (dist[s]+w < dist[t]) {		// if going to T through S is cheaper
-			dist[t] = dist[s]+w;			// update dist
-			pred[t] = s;					// update pred = S
+		if (t !within vSet || dist[s]+w < dist[t]) {	// if going to T through S is cheaper
+			dist[t] = dist[s]+w;							// update dist
+			pred[t] = s;									// update pred = S
 		}
 		vSet += t;	// add T to vSet
 		eSet -= e;	// remove edge from eSet
