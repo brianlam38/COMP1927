75f16de2bac9c0e2d574be046a27f62a3374e452
brianlam38 <brian.lam38@gmail.com>
2016-11-14 20:44:30 +1100

    2013 short ans qns

--- FINALS/FINALS.c
+++ FINALS/FINALS.c
@@ -72,6 +72,32 @@ void bfs(Graph g, Vertex v) {
 	}
 }
 
+// DFS Recursive: Modified for Connected Components
+void dfs(Graph g) {
+	int i;
+	visited = calloc(nV(g), sizeof(int));
+	order = 1;
+	while (order < nV(g)) {				// When order = nV, we have visited all vertices
+		Vertex v;
+		for (v = 0; v < nV(g); v++)		// Check all vertices to see if they are visited
+			if (visited[v] == 0)		// If not, do a recursive DFS
+				break;
+		dfsR(g,v);
+	}
+}
+void dfsR(Graph g, Vertex x) {
+	visited[v] = order++;				// Mark curr v as visited / the order visited			
+	Vertex w;
+	for (w = 0; w < nV(g); w++) {		// Iterate through possible neighbours W
+		if (!hasEdge(g,v,w))				// For each W, check if there is an edge btwn
+			continue;						// vertex V and W. Continue if no edge exists
+		if (!visited[w])				// If W has not been visited, recursively visit W
+			dfsR(g,w);						// Recursion will iterate through all possible	
+	}										// neighbours Y. Once all neighours Y are visited
+}											// then recursion will back-track one level
+
+// DJIKSTRA'S ALGORITHM
+
 // ###################
 // B S T r e e s
 // ###################
